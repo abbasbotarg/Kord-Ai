18b151a68ce6eb63ba4a74bb1377a5914ddd33b1
abbasbotarg <abbasbotarg@gmail.com>
2025-07-15 15:00:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'kord_ai-qfJHwp5JOmBT2t2I',
+    SESSION_ID: process.env.SESSION_ID || 'kord_ai-77kcnCDV9W2zL6Bt',
     OWNER_NUMBER: process.env.OWNER_NUMBER || "2349044907949",
     WORKTYPE: process.env.WORKTYPE || "private",
     PREFIX: process.env.PREFIX || "[.]",
